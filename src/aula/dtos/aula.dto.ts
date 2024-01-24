a98a18c7ec1f9f3c53f8e28b4ad71946ecd26901
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2024-01-24 14:41:54 -0300

    aula

--- src/aula/dtos/aula.dto.ts
+++ src/aula/dtos/aula.dto.ts
@@ -3,7 +3,7 @@ import { AulaMessagesHelper } from '../helpers/messages.helper';
 
 
 export class AulaDto {
-  @MinLength(2, { message: AulaMessagesHelper.AULA_NAME_NOT_VALID })
+  @MinLength(2,{ message: AulaMessagesHelper.AULA_NAME_NOT_VALID })
   name_aula: string;
 
   @MinLength(2, { message: AulaMessagesHelper.AULA_DESCRICAO_NOT_VALID })
